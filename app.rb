fc91ea07cab3dbfe8be041d61e8329d6ccd0acb7
siziy <siziy29@gmail.com>
2015-12-21 14:54:06 +0600

    Add datetimepicker plugin

--- app.rb
+++ app.rb
@@ -4,19 +4,19 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
-configure do
-	db = get_db
-	db.execute 'CREATE TABLE IF NOT EXISTS 
-		"Users" 
-		(
-			"id" INTEGER PRIMARY KEY AUTOINCREMENT, 
-			"userName" TEXT, 
-			"phone" TEXT, 
-			"dateTime" TEXT, 
-			"barber" TEXT, 
-			"color" TEXT
-		)'
-end
+# configure do
+# 	db = get_db
+# 	db.execute 'CREATE TABLE IF NOT EXISTS 
+# 		"Users" 
+# 		(
+# 			"id" INTEGER PRIMARY KEY AUTOINCREMENT, 
+# 			"userName" TEXT, 
+# 			"phone" TEXT, 
+# 			"dateTime" TEXT, 
+# 			"barber" TEXT, 
+# 			"color" TEXT
+# 		)'
+# end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -53,7 +53,7 @@ post '/visit' do
 
 	db = get_db
 	db.execute 'insert into 
-		Users 
+		"Users" 
 		(
 			userName,  
 			phone, 
